50f087ee9b8a0e3855000fbb3d8f92ebeca66bd1
daws-100s <narayanalaxmanmangena@gmail.com>
2025-11-19 10:28:46 +0530

    expense-infra

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -1,11 +1,6 @@
-resource "aws_key_pair" "eks" {
-  key_name   = "eks"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDfR/vqwOF+eA1OW7T36OrIN5xOzezKGRmxw0vLBSl84wL9c5Mpx5nooZP8EHPSxtqkD8M4LK6UZithshTQAkJuTcVZHLlOEEyZqITVUgJWMxnVl3ZneiAlid/KoR5WdgY6qJm7AqGAzbCS08WH6WZLaWKZQNEM2m140AUJXLmrvTJAWpOOqlj3pjBPpFhmzRxqvlWK8i9kLI+2PBPD28Wc3TNaN8JyeGFRgPEyGAdxhfMFA0lO/VrASZNlSNhQReTb1o+itbTqpp9DfPWIBLdZkbfcrkrQJyzkbEB3W0XBfCinkElbmrbq3HKBwQqODLRt3sTBacVCDvXTbEQVedmgtGEJ3wd58/BwyoRCBEuLXWHh+8AoxV/yIei4bXQx1JydatnmmJdyhy6TrVxEfo4s4hGob4lm+igyMTrSRI6HzmqRoBj0/Q5qWs6LRdVfVX2nBQrSwoXREPQk8ShjlBWRGWlT0zdVXkTgC+yoOVdwVvvjazO0ihugsKV+LHUe3NFgenOti0iBQzIz6Og+gbMqXO1ZuJDINJd30fWZwo9PtU2xRguWg4cFprKJ+idHIFGxTAsHyJxgAPncpk0V3bXRJACmuN5Wdhk6TEhVJ+2amMAM2vg9adZfpJCXW/mOw1reVb43yGLe2MbzdQHllgTZ8gQEPw61iTybFBM91UV7iw== ec2-user"
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.9.0"
+  version = "20.24.0"
 
   cluster_name    = "${var.project_name}-${var.environment}"
   cluster_version = "1.31"
@@ -29,7 +24,6 @@ module "eks" {
   create_node_security_group = false
   node_security_group_id     = local.node_sg_id
 
-  # Default configs for node groups
   eks_managed_node_group_defaults = {
     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
   }
@@ -39,7 +33,6 @@ module "eks" {
       min_size     = 3
       max_size     = 10
       desired_size = 3
-
       capacity_type = "SPOT"
 
       iam_role_additional_policies = {
