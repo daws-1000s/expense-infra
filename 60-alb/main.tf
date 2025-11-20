c861bae6bc70aaa3c980c71be4286f6f27246038
daws-100s <narayanalaxmanmangena@gmail.com>
2025-11-20 11:31:19 +0530

    expense-infra

--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -2,12 +2,13 @@ module "ingress_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   internal = false
-  name    = "${local.resource_name}-ingress-alb" #expense-dev-app-alb
+  name    = "${local.resource_name}-ingress-alb"
   vpc_id  = local.vpc_id
   subnets = local.public_subnet_ids
   security_groups = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
   create_security_group = false
   enable_deletion_protection = false
+
   tags = merge(
     var.common_tags,
     var.ingress_alb_tags
@@ -30,37 +31,19 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = module.ingress_alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = local.https_certificate_arn
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/html"
-      message_body = "<h1>Hello, I am from Web ALB HTTPS</h1>"
-      status_code  = "200"
-    }
-  }
-}
-
-
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "3.0.0"
 
-  zone_name = var.zone_name #daws81s.online
+  zone_name = var.zone_name
+
   records = [
     {
-      name    = "expense-${var.environment}" 
+      name    = "expense-${var.environment}"
       type    = "A"
       alias   = {
         name    = module.ingress_alb.dns_name
-        zone_id = module.ingress_alb.zone_id # This belongs ALB internal hosted zone, not ours
+        zone_id = module.ingress_alb.zone_id
       }
       allow_overwrite = true
     }
@@ -75,20 +58,20 @@ resource "aws_lb_target_group" "expense" {
   target_type = "ip"
 
   health_check {
-    healthy_threshold = 2
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    interval = 5
-    matcher = "200-299"
-    path = "/"
-    port = 8080
-    protocol = "HTTP"
-    timeout = 4
+    interval            = 5
+    matcher             = "200-299"
+    path                = "/"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 4
   }
 }
 
 resource "aws_lb_listener_rule" "frontend" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 100 # low priority will be evaluated first
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
 
   action {
     type             = "forward"
@@ -97,7 +80,7 @@ resource "aws_lb_listener_rule" "frontend" {
 
   condition {
     host_header {
-      values = ["expense-${var.environment}.${var.zone_name}"] #expense-dev.daws81s.online
+      values = ["expense-${var.environment}.${var.zone_name}"]
     }
   }
 }
